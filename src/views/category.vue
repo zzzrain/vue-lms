d36816b1e4c84bee0dba9b3e86985c7301a86eab
huzirong <1594087542@qq.com>
2019-04-15 18:23:04 +0800

    feat: 类目新增修改

--- src/views/category.vue
+++ src/views/category.vue
@@ -21,14 +21,12 @@
         title="新增类目"
         @on-ok="updateSubmitAdd"
         @on-cancel="cancel">
-        <Form  abel-position="left" :label-width="50" ref="addForm" :model="addCategory" :rules="rules">
+        <Form  abel-position="left" :label-width="50" ref="addCategory" :model="addCategory" :rules="rules">
           <Form-item label="名称" prop="name">
             <Input placeholder="请输入" v-model="addCategory.name"></Input>
           </Form-item>
           <Form-item label="等级" prop="level">
             <Select placeholder="请选择" v-model="addCategory.level">
-              <Option value="1">一级</Option>
-              <Option value="1">一级</Option>
               <Option value="1">一级</Option>
             </Select>
           </Form-item>
@@ -39,7 +37,7 @@
         title="修改类目"
         @on-ok="updateSubmitAlt"
         @on-cancel="cancel">
-        <Form label-position="left" :label-width="50" ref="addForm" :model="altCategory" :rules="rules">
+        <Form label-position="left" :label-width="50" ref="altCategory" :model="altCategory" :rules="rules">
           <Form-item label="名称" prop="name">
             <Input placeholder="请输入" v-model="altCategory.name"></Input>
           </Form-item>
@@ -52,57 +50,6 @@
         <Page :total="total" show-elevator @on-change="changePage"></Page>
       </div>
     </div>
-    <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
-      <Form-item label="姓名" prop="name">
-        <Input v-model="formValidate.name" placeholder="请输入姓名"></Input>
-      </Form-item>
-      <Form-item label="邮箱" prop="mail">
-        <Input v-model="formValidate.mail" placeholder="请输入邮箱"></Input>
-      </Form-item>
-      <Form-item label="城市" prop="city">
-        <Select v-model="formValidate.city" placeholder="请选择所在地">
-          <Option value="beijing">北京市</Option>
-          <Option value="shanghai">上海市</Option>
-          <Option value="shenzhen">深圳市</Option>
-        </Select>
-      </Form-item>
-      <Form-item label="选择日期">
-        <Row>
-          <Col span="11">
-            <Form-item prop="date">
-              <Date-picker type="date" placeholder="选择日期" v-model="formValidate.date"></Date-picker>
-            </Form-item>
-          </Col>
-          <Col span="2" style="text-align: center">-</Col>
-          <Col span="11">
-            <Form-item prop="time">
-              <Time-picker type="time" placeholder="选择时间" v-model="formValidate.time"></Time-picker>
-            </Form-item>
-          </Col>
-        </Row>
-      </Form-item>
-      <Form-item label="性别" prop="gender">
-        <Radio-group v-model="formValidate.gender">
-          <Radio label="male">男</Radio>
-          <Radio label="female">女</Radio>
-        </Radio-group>
-      </Form-item>
-      <Form-item label="爱好" prop="interest">
-        <Checkbox-group v-model="formValidate.interest">
-          <Checkbox label="吃饭"></Checkbox>
-          <Checkbox label="睡觉"></Checkbox>
-          <Checkbox label="跑步"></Checkbox>
-          <Checkbox label="看电影"></Checkbox>
-        </Checkbox-group>
-      </Form-item>
-      <Form-item label="介绍" prop="desc">
-        <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="请输入..."></Input>
-      </Form-item>
-      <Form-item>
-        <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
-        <Button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
-      </Form-item>
-    </Form>
   </div>
 </template>
 
@@ -138,45 +85,6 @@ export default {
         ],
         level: [
           {required: true, message: '请选择等级', trigger: 'change'}
-        ]
-      },
-      formValidate: {
-        name: '',
-        mail: '',
-        city: '',
-        gender: '',
-        interest: [],
-        date: '',
-        time: '',
-        desc: ''
-      },
-      ruleValidate: {
-        name: [
-          { required: true, message: '姓名不能为空', trigger: 'blur' }
-        ],
-        mail: [
-          { required: true, message: '邮箱不能为空', trigger: 'blur' },
-          { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
-        ],
-        city: [
-          { required: true, message: '请选择城市', trigger: 'change' }
-        ],
-        gender: [
-          { required: true, message: '请选择性别', trigger: 'change' }
-        ],
-        interest: [
-          { required: true, type: 'array', min: 1, message: '至少选择一个爱好', trigger: 'change' },
-          { type: 'array', max: 2, message: '最多选择两个爱好', trigger: 'change' }
-        ],
-        date: [
-          { required: true, type: 'date', message: '请选择日期', trigger: 'change' }
-        ],
-        time: [
-          { required: true, type: 'date', message: '请选择时间', trigger: 'change' }
-        ],
-        desc: [
-          { required: true, message: '请输入个人介绍', trigger: 'blur' },
-          { type: 'string', min: 20, message: '介绍不能少于20字', trigger: 'blur' }
         ]
       }
     };
@@ -279,7 +187,7 @@ export default {
         .catch(error => console.log(error));
     },
     updateSubmitAdd () {
-      this.$refs.addForm.validate((valid) => {
+      this.$refs.addCategory.validate((valid) => {
         if (valid) {
           this.$axios
             .post('/api/lms/admin/category/updateCategory', {
@@ -298,7 +206,7 @@ export default {
       });
     },
     updateSubmitAlt () {
-      this.$refs.addForm.validate((valid) => {
+      this.$refs.altCategory.validate((valid) => {
         if (valid) {
           this.$axios
             .post('/api/lms/admin/category/updateCategory', {
@@ -314,15 +222,6 @@ export default {
               }
             })
             .catch(error => console.log(error));
-        }
-      });
-    },
-    handleSubmit (name) {
-      this.$refs[name].validate((valid) => {
-        if (valid) {
-          this.$Message.success('提交成功!');
-        } else {
-          this.$Message.error('表单验证失败!');
         }
       });
     },
--- src/views/stock.vue
+++ src/views/stock.vue
@@ -1,12 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <Button type="primary" @click="modal6 = true">显示对话框</Button>
+    <Modal
+      v-model="modal6"
+      title="对话框标题"
+      :loading="loading"
+      @on-ok="asyncOK">
+      <p>点击确定后，对话框将在 2秒 后关闭。</p>
+    </Modal>
+  </div>
 </template>
-
 <script>
 export default {
+  data () {
+    return {
+      modal6: false,
+      loading: true
+    };
+  },
+  methods: {
+    asyncOK () {
+      setTimeout(() => {
+        this.modal6 = false;
+      }, 2000);
+    }
+  }
 };
 </script>
-
-<style scoped>
-
-</style>
